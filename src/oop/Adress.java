0a3f3ea3a390d6b7a8e0e61dd55bd3613ed4800d
George Sydby <george.sydby@gmail.com>
2019-01-16 13:33:15 +0100

    Skapade Adress Class efter lunch

---
+++ src/oop/Adress.java
@@ -0,0 +1,39 @@
+package oop;
+
+public class Adress {
+
+    private String gata;
+    private String postnummer;
+    private String ort;
+
+    //Constructor
+    public Adress(String gata, String postnummer, String ort) {
+        this.gata = gata;
+        this.postnummer = postnummer;
+        this.ort = ort;
+    }
+
+    public String getGata() {
+        return gata;
+    }
+
+    public void setGata(String gata) {
+        this.gata = gata;
+    }
+
+    public String getPostnummer() {
+        return postnummer;
+    }
+
+    public void setPostnummer(String postnummer) {
+        this.postnummer = postnummer;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
+}
--- src/oop/Matematik.java
+++ src/oop/Matematik.java
@@ -8,13 +8,19 @@ public class Matematik {
 
         Course matematik = new Course(10, 92, "Matematik",  teacher);
 
+        Adress plats = new Adress("Sandspåret 23", "424 31", "ANGERED");
+
         System.out.println("Pupils: " + matematik.getPupils() );
         System.out.println("Hours: " + matematik.getHours() );
         System.out.println("Kurs: " + matematik.getKurs() );
-
-        //Course.getTeacher().getName();
+        System.out.println();
 
         System.out.println(matematik.getTeacher().getName().charAt(0));
+
+        System.out.println(matematik.getTeacher().getName());
+        System.out.println(plats.getGata());
+        System.out.print(plats.getPostnummer()+" ");
+        System.out.println(plats.getOrt());
 
     }
 }
